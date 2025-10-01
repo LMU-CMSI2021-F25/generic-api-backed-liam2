fc86468908eef7c7db2a566bcdfb94e97dcada47
Liam Nagel <>
2025-10-01 14:01:24 -0700

    fixes

--- project-liam/src/App.jsx
+++ project-liam/src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [collapsed, setCollapsed] = useState(false)
+  const [collapsed, setCollapsed] = useState()
   const [movies, setMovies] = useState([])
   const sidebarClass = collapsed ? "sidebar collapsed" : "sidebar"
   const mainContent = collapsed ? "main-content collapsed" : "main-content"
@@ -26,7 +26,6 @@ function App() {
           top_ten.push(my_dict)
         }
         setMovies(top_ten)
-        console.log(top_ten)
         } catch (err) {
         console.error("Failed to fetch posts", err)
         }
@@ -54,18 +53,34 @@ function App() {
         }
   }
 
-   
-  const displayFilms = (movie) => {
-    return (
-      <div key={movie.id} className='content-box'>
-        <a className='content-box-title'>{movie.title}</a>
-        <img className='content-box-poster' src={movie.image}></img>
-      </div>
-    )}
+  const searchMovie = async (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target);
+    const query = formData.get("query")
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`)
+      if (!response.ok) {
+        throw new Error(`Response Status: ${response.status}`)
+      }
+      const result = await response.json()
+      const top_ten = []
+      for (let i = 0; i < 10; i++) {
+        const data = result.results[i]
+        const my_dict = {}
+        my_dict['image'] = `https://image.tmdb.org/t/p/w500${data.poster_path}`
+        my_dict['title'] = data.original_title
+        top_ten.push(my_dict)
+      }
+      setMovies(top_ten)
+    } 
+    catch (err) {
+      console.error("Failed", err)
+    }
+  }
 
-
-
-
+  useEffect( () => {
+    fetchPopularMovies()
+  }, [])
 
   return (
     <div className="app">
@@ -73,7 +88,7 @@ function App() {
         <button className="sidebar-collapse-button" onClick={ () => setCollapsed(!collapsed)}></button>
         <div className="sidebar-content">
           <div className="sidebar-item">
-            <button className="sidebar-button" onClick={() => fetchPopularMovies("popular")}></button>
+            <button className="sidebar-button" onClick={() => fetchPopularMovies()}></button>
             <a className={sidebarItemText}>Ratings</a>
           </div>
           <div className="sidebar-item">
@@ -88,15 +103,23 @@ function App() {
             <button className="sidebar-button" onClick={ () => fetchMovieGenre("27")}></button>
             <a className={sidebarItemText}>Horror</a>
           </div>
+          <form className="sidebar-item" onSubmit={ (e) => searchMovie(e) }>
+            <label className={sidebarItemText}>Search: <input name="query"></input></label>
+          </form>
         </div>
       </div>
       <div className={mainContent}>
         <div className="content-header">
           <h1 className='title'>The Movie Website</h1>
         </div>
-        <div className="content-apps">
-          {movies.map(displayFilms)}
-        </div>
+      <div className="content-apps">
+        {movies.map((movie, index) => (
+          <div key={index} className="content-box">
+            <a className="content-box-title">{movie.title}</a>
+            <img className="content-box-poster" src={movie.image} alt={movie.title} />
+          </div>
+        ))}
+      </div>
       </div>
     </div>
   )
